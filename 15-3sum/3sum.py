e0cd65e16233f2d991cc595ffef7b40daaf319ef
Purushothamcv <purushothamcv18@gmail.com>
2025-06-29 00:27:06 +0530

    Time: 665 ms (79.85%) | Memory: 18.2 MB (94.40%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -23,6 +23,8 @@ class Solution(object):
                
                     while (left<right) and nums[left]==nums[left-1]:
                         left+=1
+                    while (left<right) and nums[right]==nums[right-1]:
+                        right-=1
         return triplets
                 
 
