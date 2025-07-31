e9675dae11991edaad07e89d11e91321b5cff05d
Purushothamcv <purushothamcv18@gmail.com>
2025-07-31 22:45:44 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (27.36%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -4,5 +4,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return min(nums)
+        left=0
+        right=len(nums)-1
+        while left<right:
+            mid=(left+right)//2
+            if nums[mid]<nums[right]:
+                right=mid
+            else:
+                left=mid+1
+        return nums[right]      
+            # if nums[left] == nums[mid] == nums[right]:
+            #     left += 1
+            #     right -= 1
+            # elif nums[left]<=nums[mid]:
+            #     if nums[left]<=target<nums[mid]:
+            #         right=mid-1
+            #     else:
+            #         left=mid+1
+            # else:
+            #     if nums[mid]<target<=nums[right]:
+            #         left=mid+1
+            #     else:
+            #         right=mid-1
+        # return False
+
+
         
