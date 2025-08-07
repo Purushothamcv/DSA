db2d5eafda58f8b23c80d98d42228646ad87658e
Purushothamcv <purushothamcv18@gmail.com>
2025-08-08 02:19:37 +0530

    Time: 4 ms (79.44%) | Memory: 13.7 MB (10.55%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,14 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        n=len(nums)
+        # n=len(nums)
     
+        # left=0
+        # right=0
+        # for right in range(n):
+        #     if nums[right]!=0:
+        #         nums[right],nums[left]=nums[left],nums[right]
+        #         right+=1
+        #         left+=1
+        #     right+=1
+        # return nums
+        a=[]
         left=0
-        right=0
-        for right in range(n):
-            if nums[right]!=0:
-                nums[right],nums[left]=nums[left],nums[right]
-                right+=1
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[left],nums[i]=nums[i],nums[left]
                 left+=1
-            right+=1
         return nums
+        
