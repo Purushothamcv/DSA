f6e047086687e9111d103a361a4d856c20daf772
Purushotham C V <purushothamcv18@gmail.com>
2026-07-25 13:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (79.16%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -4,11 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        a={}
-        for i in nums:
-            a[i]=a.get(i,0)+1
-        for i,value in a.items():
-            if a[i]==1:
-                return i
-        
+        result=0
+        for num in nums:
+            result^=num
+        return result
         
