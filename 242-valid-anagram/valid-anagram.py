b5a67da775e757fbb44c2a60be363a611205f120
Purushotham C V <purushothamcv18@gmail.com>
2026-07-31 01:00:32 +0530

    Time: 25 ms (32.46%) | Memory: 13.4 MB (40.21%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,15 +5,4 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        a={}
-        b={}
-        for i in range(len(s)):
-            a[s[i]]=a.get(s[i],0)+1
-        for j in range(len(t)):
-            b[t[j]]=b.get(t[j],0)+1
-        # print(a)
-        if a==b:
-            return True
-        else:
-            return False
-        
+        return sorted(s)==sorted(t)
